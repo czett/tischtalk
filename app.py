b17742cdcc730a3cae5e1a2a320979b762a0dc6b
czett <keinaniemand@gmail.com>
2024-11-05 19:34:27 +0100

    database flimb

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import random
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///votes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
 db = SQLAlchemy(app)
 app.secret_key = "gleezeborpglorpzyblopglorporbleflimb"
 
