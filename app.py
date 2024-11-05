dd3b683598b74d9b5e748eb1d25a2fc19c96b527
czett <keinaniemand@gmail.com>
2024-11-05 19:26:46 +0100

    done? hopefully

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, redirect, request, session
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import random
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///votes.db'
 db = SQLAlchemy(app)
 app.secret_key = "gleezeborpglorpzyblopglorporbleflimb"
+
+last_run_date = None
+current_question = None
 
 class Vote(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,13 +19,31 @@ class Vote(db.Model):
 with app.app_context():
     db.create_all()
 
-# Liste der Personen
-persons = ['Adrian', 'Anna', 'Annika', 'Bjarne', 'Connor', 'Eike', 'Elias', 'Finn', 'Jara', 'Johannes', 'Joseph', 'Lilith', 'Richard', 'Simon', 'Timo']
+def check_new_day():
+    global last_run_date
+    current_date = datetime.now().date()
+    
+    if last_run_date is None or current_date > last_run_date:
+        last_run_date = current_date
+        return True
+    return False
+
+def persons() -> list:
+    with open("static/persons.csv", "r") as file:
+        return [person for person in file.readline().split(",")]
+    
+def new_question():
+    global current_question
+    with open("static/questions.csv", "r") as file:
+        questions = [q for q in file.readline().split(";")]
+        current_question = random.choice(questions)
 
 @app.route("/")
 def start():
     if session:
-        return render_template("index.html", persons=persons, li=session["logged_in"], voted=session["voted"])
+        if check_new_day():
+            new_question()
+        return render_template("index.html", persons=persons(), li=session["logged_in"], voted=session["voted"], q=current_question)
     else:
         return redirect("/session_config")
     
@@ -56,16 +79,27 @@ def vote_handling():
 def results():
     votes = Vote.query.order_by(Vote.count.desc()).all()
     total_votes = db.session.query(db.func.sum(Vote.count)).scalar() or 0
-    return render_template("results.html", votes=votes, total_votes=total_votes)
+    return render_template("results.html", votes=votes, total_votes=total_votes, q=current_question)
 
-@app.route("/reset_votes")
+@app.route("/reset")
 def reset_votes():
+    global last_run_date
+    global current_question
+    
+    last_run_date = None
+    current_question = None
+
     db.session.query(Vote).delete()
     db.session.commit()
 
     session["voted"] = False
     session.modified = True
 
+    return redirect("/")
+
+@app.errorhandler(Exception)
+def handle_error(error):
+    # Error handling will be done, however procrastinated :3
     return redirect("/")
 
 if __name__ == "__main__":
