a91e8e1f6c4418c1bb47edae019e7823962c65a0
czett <keinaniemand@gmail.com>
2024-11-07 19:04:24 +0100

    aargh

--- app.py
+++ app.py
@@ -9,11 +9,6 @@ load_dotenv()  # Load environment variables from .env file
 app = Flask(__name__)
 app.secret_key = os.getenv("FLASK_SECRET_KEY", "default_secret_key")  # Retrieve secret key from env
 
-# Ensure database URI is loaded from the environment variables
-SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
-if not SQLALCHEMY_DATABASE_URI:
-    raise RuntimeError("SQLALCHEMY_DATABASE_URI is not set in environment variables!")
-
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres.ymjaliwvpuvhkevhguwd:xf4CKBawMd-q2Q*@aws-0-eu-central-1.pooler.supabase.com:6543/postgres"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
